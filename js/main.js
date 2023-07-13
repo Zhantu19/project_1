1745631c04901c6b16de2c5d478112c19369b1c4
Zhantu <exaple@gmail.com>
2023-07-13 14:29:44 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+// const result=confirm("Are you here");
+
+// const answer=+prompt("Вам есть 18?","18");
+// console.log(answer + 10);
+
+// const answers=[];
+
+// // answers[0]=prompt('Ваше имя ?','');
+// // answers[1]=prompt('Вашe фамилия ?','');
+// // answers[2]=prompt('Ваш возраст ?','');
+
+// console.log(typeof(answers));
+
+// const category='toys';
+
+// console.log(`https://someurl.com/s{category}/5`);
+
+// const user="Kai";
+
+// alert(`Привет,${user}`);
+
+console.log('arr' + "- object");
+console.log(4 + +"5");
+
+let incr=10,
+    decr=10;
+
+incr++;
+decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 !='6');
+
+const isChecked=false,
+      isClose=false;
+      
+console.log(isChecked || isClose);
